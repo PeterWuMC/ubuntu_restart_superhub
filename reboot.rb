32457100181a734308b37ef85392e7f927e9c0b1
Peter Wu <peter.wu@xbridge.com>
2012-12-31 08:31:06 +0000

    using log warn when internet connectivity is lost

--- reboot.rb
+++ reboot.rb
@@ -10,14 +10,14 @@ ping_count = 5
 server = "4.2.2.2"
 result = `ping -q -c #{ping_count} #{server}`
 unless ($?.exitstatus == 0)
-  logger.debug "[#{Time.now.utc.strftime("%Y/%m/%d %H:%M:%S")}] ...RESTARTING SUPERHUB"
+  logger.warn "[#{Time.now.utc.strftime("%Y/%m/%d %H:%M:%S")}] ...RESTARTING SUPERHUB"
   begin
     Capybara.default_driver = :selenium
     page.visit("http://activation.virginmedia.com/Activation/")
     page.find(:css, "#continueButtonBig").click
     sleep 2
   rescue => e
-    logger.debug "[#{Time.now.utc.strftime("%Y/%m/%d %H:%M:%S")}] ...Something is wrong - #{e.class}: #{e.message}: #{e.backtrace}"
+    logger.error "[#{Time.now.utc.strftime("%Y/%m/%d %H:%M:%S")}] ...Something is wrong - #{e.class}: #{e.message}: #{e.backtrace}"
   end
 end
 logger.debug "[#{Time.now.utc.strftime("%Y/%m/%d %H:%M:%S")}] END"
