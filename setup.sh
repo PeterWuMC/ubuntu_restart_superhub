7578277c6df19b080c624987d9cf65c2f1544412
System <system@raspberrypi.(none)>
2013-03-09 19:03:23 +0000

    updated the setup script

--- setup.sh
+++ setup.sh
@@ -2,6 +2,6 @@ bundle install
 
 #insertting it into your crontab
 crontab -l > mycron
-echo "*/30 * * * * DISPLAY=localhost:1.0 /bin/bash -c 'source $HOME/.rvm/scripts/rvm && cd $PWD && ruby -rubygems reboot.rb'" >> mycron
+echo "*/30 * * * * /bin/bash -c 'source $rvm_path/scripts/rvm && cd $PWD && ruby -rubygems reboot.rb'" >> mycron
 crontab mycron
 rm mycron
